4a4d283d301ba13d314f9cb3fecf4011ca17d86c
WillBurdett <williamburdett1995@gmail.com>
2023-01-08 13:01:41 +0000

    model and get method added

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,76 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public CurrencyExchange() {
+    }
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CurrencyExchange that = (CurrencyExchange) o;
+        return Objects.equals(id, that.id) && Objects.equals(from, that.from) && Objects.equals(to, that.to) && Objects.equals(conversionMultiple, that.conversionMultiple);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, conversionMultiple);
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyExchange{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionMultiple=" + conversionMultiple +
+                '}';
+    }
+}
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -11,3 +11,14 @@ public class CurrencyExchangeServiceApplication {
 	}
 
 }
+
+// Currency Exchange Service
+//http://localhost:8000/currency-exchange/from/USD/to/INR
+//
+//{
+//		"id":10001,
+//		"from":"USD",
+//		"to":"INR",
+//		"conversionMultiple":65.00,
+//		"environment":"8000 instance-id"
+//		}
