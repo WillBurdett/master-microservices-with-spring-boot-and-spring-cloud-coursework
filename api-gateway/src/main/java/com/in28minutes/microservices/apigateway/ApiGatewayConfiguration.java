6262ccf8bf3cea655c75f56896f8a37d97f27433
WillBurdett <williamburdett1995@gmail.com>
2023-02-04 14:56:48 +0000

    custom urls added, url rewrite, and urls doc added

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -14,14 +14,26 @@ public class ApiGatewayConfiguration {
 
   @Bean
   public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-    Function<PredicateSpec,
-        Buildable<Route>> routeFunction
-          = p -> p.path("/get")
-        .filters(f -> f
-            .addRequestHeader("MyHeader", "MyURI"))
-        .uri("http://httpbin.org:80");
     return builder.routes()
-        .route(routeFunction)
+        .route(p -> p.path("/get")
+            .filters(f -> f
+                .addRequestHeader("MyHeader", "MyURI")
+                .addRequestParameter("Param", "MyValue "))
+             .uri("http://httpbin.org:80"))
+        // anything with this url pattern
+        .route(p -> p.path("/currency-exchange/**")
+            // should get sent to this name on eureka server, load balance
+            .uri("lb://currency-exchange"))
+        .route(p -> p.path("/currency-conversion/**")
+            .uri("lb://currency-conversion"))
+        .route(p -> p.path("/currency-conversion-feign/**")
+            .uri("lb://currency-conversion"))
+        .route(p -> p.path("/currency-conversion-new/**")
+            .filters(f -> f.rewritePath(
+                "/currency-conversion-new/(?<segment>.*)",
+                "/currency-conversion-feign/${segment}"
+            ))
+            .uri("lb://currency-conversion"))
         .build();
   }
 }
