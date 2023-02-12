c9edfb2f700329c05bdd254659d70ccea5ce1fad
WillBurdett <williamburdett1995@gmail.com>
2023-02-12 12:13:24 +0000

    fallback response added on x ampount of retries

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -14,12 +14,18 @@ public class CircuitBreakerController {
  private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
  @GetMapping("sample-api")
  //@Retry(name = "default")
- @Retry(name = "sample-api")
+ @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
   public String sampleApi(){
   logger.info("Sample api call received");
   ResponseEntity<String> forEntity =
       new RestTemplate()
           .getForEntity("http://localhost:8080/some-dummy-url", String.class);
   return forEntity.getBody();
+ }
+
+ // must handle throwable (by extending exception)
+ // you can handle different exceptions with different responses
+ public String hardcodedResponse(Exception ex){
+   return "fallback-response";
  }
 }
