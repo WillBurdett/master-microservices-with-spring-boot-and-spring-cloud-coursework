c6a99df9ecdb1398ff6819ff773478c2878332b4
WillBurdett <williamburdett1995@gmail.com>
2023-03-03 14:56:44 +0000

    finished circuit breaker section

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +17,23 @@ public class CircuitBreakerController {
  private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
  @GetMapping("sample-api")
  //@Retry(name = "default")
- @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+ //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+
+// At what given percentage of successful API calls (90%) will I begin returning a default response (set to 'OPEN')
+ //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+
+ // in 10s I only want to allow 10_000 calls, specific to this API call
+ //@RateLimiter(name = "default")
+
+ // how many concurrent calls do you want to allow
+ @Bulkhead(name = "sample-api")
   public String sampleApi(){
   logger.info("Sample api call received");
-  ResponseEntity<String> forEntity =
-      new RestTemplate()
-          .getForEntity("http://localhost:8080/some-dummy-url", String.class);
-  return forEntity.getBody();
+//  ResponseEntity<String> forEntity =
+//      new RestTemplate()
+//          .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//  return forEntity.getBody();
+   return "sample-api";
  }
 
  // must handle throwable (by extending exception)
